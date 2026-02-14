8f391bae14c62cf49543295fa6f04c1c2f269145
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2026-02-13 18:42:31 -0600

    feat: Implement user service with authentication and profile retrieval functionality

--- services/auth-service/src/auth/auth.controller.ts
+++ services/auth-service/src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { UseGuards, Get, Req }  from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 
 @Controller('auth')
@@ -17,5 +19,10 @@ register(@Body() body: RegisterDto) {
 login(@Body() body: LoginDto) {
   return this.authService.login(body.email, body.password);
 }
+@UseGuards(AuthGuard('jwt'))
+@Get('profile')
+getProfile(@Req() req) {
+  return req.user;  
 
+}
 }
--- services/auth-service/src/auth/auth.module.ts
+++ services/auth-service/src/auth/auth.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from '../infrastructure/database/entities/user.entity';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule,
     JwtModule.register({
       secret: 'supersecret',
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
